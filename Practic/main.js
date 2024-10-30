000b5ac153b73a56eb9c0b2144b4f7e2cbbca562
TimofeyMikhalev <79932412+TimofeyMikhalev@users.noreply.github.com>
2024-10-31 00:53:33 +0200

    Finish project

--- Practic/main.js
+++ Practic/main.js
@@ -157,3 +157,49 @@ function findOldest(people) {
 } 
 
 findOldest(arr).name
+
+
+///Самостоятельная работа
+
+let crestic = document.querySelectorAll('.crestic');
+let film = document.querySelectorAll('.film');
+let spisocFilms = document.querySelector('.spisoc__films');
+let inputAdd = document.querySelector('.search__films');
+let btnInput = document.querySelector('.search__btn');
+
+
+// Удаление фильма из списка
+spisocFilms.addEventListener('click', function(e) {
+    const btn = e.target.closest('.crestic');
+    if (btn) {
+        btn.parentElement.remove(); // Удаляет родительский элемент <li> при клике на crestic
+    }
+});
+
+
+// Ставим на фильме знак "прочитано" (вешаем активный класс)
+spisocFilms.addEventListener('click', function(e) {
+    const film = e.target.closest('.film');
+    if (film && !e.target.closest('.crestic')) {
+        film.classList.toggle('ready'); // Переключаем класс "ready" для элемента <li>
+    }
+});
+
+
+//Добавление фильма в список
+function inputSend(e) {
+    e.preventDefault();
+
+    // HTML-код нового элемента li
+    let newLiHTML = `<li class="film"><span class="circle"></span>${inputAdd.value}<span class="crestic"><svg width="23" height="23" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg"><path opacity="0.3" d="M22.3575 3.74463L14.6021 11.5L22.3575 19.2554C22.7689 19.6667 23 20.2247 23 20.8064C23 21.3882 22.7689 21.9462 22.3575 22.3575C21.9462 22.7689 21.3882 23 20.8064 23C20.2247 23 19.6667 22.7689 19.2554 22.3575L11.5 14.6022L3.74463 22.3575C3.33326 22.7689 2.77532 23 2.19355 23C1.61179 23 1.05385 22.7689 0.642476 22.3575C0.231106 21.9462 7.3961e-07 21.3882 7.3961e-07 20.8064C7.3961e-07 20.2247 0.231106 19.6667 0.642476 19.2554L8.39785 11.5L0.642476 3.74463C0.231106 3.33326 0 2.77532 0 2.19355C0 1.61179 0.231106 1.05385 0.642476 0.642477C1.05385 0.231106 1.61178 7.3961e-07 2.19355 7.3961e-07C2.77532 7.3961e-07 3.33326 0.231106 3.74463 0.642477L11.5 8.39785L19.2554 0.642477C19.6667 0.231106 20.2247 0 20.8064 0C21.3882 0 21.9462 0.231106 22.3575 0.642477C22.7689 1.05385 23 1.61178 23 2.19355C23 2.77532 22.7689 3.33326 22.3575 3.74463Z" fill="#F3F6F9"/></svg></span></li>`;
+
+    // // Добавляем новый элемент в конец ul
+    // spisocFilms.innerHTML += newLiHTML;
+    // Добавляем новый элемент в конец ul
+    spisocFilms.insertAdjacentHTML('beforeend', newLiHTML);
+
+    // Очищаем input после добавления
+    inputAdd.value = "";
+}
+
+btnInput.addEventListener('click', inputSend)
